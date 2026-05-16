9e560c6f397045a5bc9639f1ff94a7d5ee60501a
Beatriz-AF <aninhalllmeida@gmail.com>
2026-05-16 15:30:46 -0300

    Html e Css

---
+++ script.js
@@ -0,0 +1,48 @@
+const ctx = document.getElementById('graficoTemperatura');
+
+new Chart(ctx, {
+    type: 'line',
+
+    data: {
+        labels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+
+        datasets: [
+            {
+                label: 'Temperatura Máxima',
+                data: [31, 30, 29, 32, 33, 31, 30],
+                borderColor: '#ffcc00',
+                backgroundColor: '#ffcc00',
+                tension: 0.4
+            },
+
+            {
+                label: 'Temperatura Mínima',
+                data: [24, 23, 22, 24, 25, 23, 22],
+                borderColor: '#00d4ff',
+                backgroundColor: '#00d4ff',
+                tension: 0.4
+            }
+        ]
+    }
+});
+
+const clima = "Nublado"; 
+ //mudar clima com API
+
+    const descricao = document.getElementById("descricao-clima");
+
+    if(clima === "Ensolarado"){
+        descricao.innerHTML = "Ensolarado ☀️";
+    }
+
+    else if(clima === "Chuva"){
+        descricao.innerHTML = "Chovendo 🌧️";
+    }
+
+    else if(clima === "Nublado"){
+        descricao.innerHTML = "Nublado ☁️";
+    }
+
+    else{
+        descricao.innerHTML = clima;
+    }
